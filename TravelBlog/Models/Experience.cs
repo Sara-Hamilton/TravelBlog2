444494efd9dd77b9e1c290219c6741e4adcadb7e
Andy Grossberg and Hamza Naeem <hamza@>
2018-04-18 11:03:32 -0700

    Add directory structure and base files

---
+++ TravelBlog/Models/Experience.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelBlog.Models
+{
+    [Table("Experiences")]
+    public class Experience
+    {
+        public Experience()
+        {
+        }
+        [Key]
+        public int ExperienceId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public DateTime TravelDate { get; set; }
+        public List<int> PersonId { get; set; }
+        public List<int> LocationId { get; set; }
+    }
+}
---
+++ TravelBlog/Models/TravelBlogContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelBlog.Models
+{
+    public class TravelBlogContext : DbContext
+    {
+        public TravelBlogContext()
+        {
+            
+        }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;Port=8889;database=travelblog;uid=root;pwd=root;");
+
+    }
+}
